a1223c6311dc7eb62b6c566822147c41ea124422
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2023-07-31 18:52:08 +0300

    signalRChat

--- SignalR_Chat/Program.cs
+++ SignalR_Chat/Program.cs
@@ -27,6 +27,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapHub<ChatHub>("/chatHub");
+app.MapHub<ChatHub>("/chatHub"); //signalar
 
 app.Run();
